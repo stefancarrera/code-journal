6033d53451cd5e56f5ef64103c05d3e4f995fa29
Stefan Carrera <stefancarrera@gmail.com>
2021-05-24 18:50:57 -0500

    Completed photoURL img src update.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 /* global data */
+
+var photoUrl = document.querySelector('#photoUrl');
+var img = document.querySelector('#photo-preview');
+// var newEntry = {};
+// var nextEntryId = 0;
+// var title = document.querySelector('#entry-title');
+// var notes = document.querySelector('#notes');
+
+photoUrl.addEventListener('input', function (event) {
+  img.src = event.target.value;
+});
+
 /* exported data */
